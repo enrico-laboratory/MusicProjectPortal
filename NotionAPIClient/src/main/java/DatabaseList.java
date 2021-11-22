55a1b635528f04ffa46c1f263664f958e47e1f01
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-11-22 12:37:19 +0100

    Remove Notion

--- NotionAPIClient/src/main/java/DatabaseList.java
+++
@@ -1,8 +0,0 @@
-public class DatabaseList {
-
-    public static final String MUSIC_PROJECT_DATABASE = "a290b3f737f242b697959acb9f18283e";
-    public static final String REPERTOIRE_DATABASE = "aaff3c7cc9fd4c49a9d0cead3d51f75b";
-    public static final String SCHEDULE_DATABASE = "b25b103631b34fc2a8804eec0b2b3813";
-    public static final String CAST_DATABASE = "065066b1d60a478a9d8a2725b2fee660";
-    public static final String LOCATION_DATABASE = "264aecbf12ec4d439a306d3360a70001";
-}

8e0c8de9257cc16f99700b51c290ff0d593f6ec2
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-12-03 18:18:58 +0100

    Implemented Main and moved to src/java

---
+++ DatabaseEngine/src/main/java/com/enricoruggieri/Main.java
@@ -0,0 +1,134 @@
+package com.enricoruggieri;
+
+import com.enricoruggieri.database_engine.Config;
+import com.enricoruggieri.database_engine.MasterTables;
+import com.enricoruggieri.database_engine.TableMaps.TableMapMusicProject;
+import com.enricoruggieri.database_engine.to_google_sheet.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws GeneralSecurityException, IOException, InterruptedException {
+
+        // Build master tables
+        var musicProjectTable = MasterTables.buildTableMusicProject();
+        var repertoireAndDivisiTable = MasterTables.buildTableRepertoireAndDivisi();
+        var scheduleTable = MasterTables.buildTableSchedule();
+        var castTable = MasterTables.buildTableCast();
+        var locationTable = MasterTables.buildTableLocation();
+
+        // List ongoing projects
+        List<TableMapMusicProject> ongoingMusicProjects = new ArrayList<>();
+        musicProjectTable.forEach(ongoingMusicProject -> {
+                    if (!ongoingMusicProject.getCancelled() && !ongoingMusicProject.getCompleted()) {
+                        ongoingMusicProjects.add(ongoingMusicProject);
+                    }
+                }
+        );
+
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar.getTime());
+
+        for (TableMapMusicProject project : ongoingMusicProjects) {
+
+            SpreadsheetCreator spreadsheet = new SpreadsheetCreator(
+                    project.getTitle(),
+                    project.getProjectId(),
+                    Config.TEST_FOLDER_ID);
+
+            System.out.println();
+            System.out.println("Deleting default Sheet1...");
+            spreadsheet.deleteDefaultSheet();
+
+            List<SheetBuilder> sheetsList = new ArrayList<>();
+
+            var sheetRepertoire = new SheetRepertoire(
+                    spreadsheet.getSpreadsheetId(),
+                    spreadsheet.getProjectId(),
+                    ValueListsBuilder.buildRepertoireValueList(
+                            spreadsheet.getProjectId(),
+                            repertoireAndDivisiTable
+                    )
+            );
+
+            var sheetSchedule = new SheetSchedule(
+                    spreadsheet.getSpreadsheetId(),
+                    spreadsheet.getProjectId(),
+                    ValueListsBuilder.buildScheduleValueList(
+                            spreadsheet.getProjectId(),
+                            scheduleTable
+                    )
+            );
+
+            var sheetCast = new SheetCast(
+                    spreadsheet.getSpreadsheetId(),
+                    spreadsheet.getProjectId(),
+                    ValueListsBuilder.buildCastValueList(
+                            spreadsheet.getProjectId(),
+                            castTable
+                    )
+            );
+
+            var sheetDivisi= new SheetDivisi(
+                    spreadsheet.getSpreadsheetId(),
+                    spreadsheet.getProjectId(),
+                    ValueListsBuilder.buildDivisiValueList(
+                            spreadsheet.getProjectId(),
+                            repertoireAndDivisiTable
+                    )
+            );
+
+            var sheetLocation= new SheetLocations(
+                    spreadsheet.getSpreadsheetId(),
+                    spreadsheet.getProjectId(),
+                    ValueListsBuilder.buildLocationValueList(
+                            spreadsheet.getProjectId(),
+                            locationTable,
+                            scheduleTable
+                    )
+            );
+
+            sheetsList.add(sheetRepertoire);
+            sheetsList.add(sheetSchedule);
+            sheetsList.add(sheetCast);
+            sheetsList.add(sheetDivisi);
+            sheetsList.add(sheetLocation);
+
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
+            System.out.println("Updating project \"" + project.getTitle() + "\"");
+            System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
+
+            for (SheetBuilder sheet : sheetsList) {
+                System.out.println();
+                System.out.println("Updating sheet: " + sheet.getSheetName().toUpperCase());
+                try {
+                    System.out.println("Creating sheets: " + sheet.addSheet());
+                } catch (NullPointerException e) {
+                    System.out.println("Sheet already exists!");
+                }
+                System.out.println("Clearing Sheet: " + sheet.clearSheet());
+                System.out.println("Getting updated cells :" + sheet.updateSheet());
+                System.out.println("Making first row bold...");
+                System.out.println(sheet.makeFirstRowBold());
+                System.out.println("Sorting sheet...");
+                System.out.println(sheet.sortSheet());
+                System.out.println("Setting column dimension to auto...");
+                System.out.println(sheet.setColumnDimensionAuto());
+                System.out.println("Changing date format...");
+                System.out.println(sheet.changeDateFormat());
+                System.out.println("Setting text wrapping to clip...");
+                System.out.println(sheet.setTextWrappingClip());
+                System.out.println("Setting columns dimension...");
+                sheet.setColumnDimension();
+            }
+
+            Thread.sleep(61000);
+        }
+        System.out.println("hello!");
+    }
+}

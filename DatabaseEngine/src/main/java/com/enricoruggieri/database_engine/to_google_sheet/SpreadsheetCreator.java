6767f7ca26065ca2f8e3a55dd8c8abd599c7cb19
Enrico Ruggieri <enr.ruggieri.mail@gmail.com>
2021-12-03 17:39:58 +0100

    SpreadsheetCreator.java implemented and tested

---
+++ DatabaseEngine/src/main/java/com/enricoruggieri/database_engine/to_google_sheet/SpreadsheetCreator.java
@@ -0,0 +1,46 @@
+package com.enricoruggieri.database_engine.to_google_sheet;
+
+import com.enricoruggieri.google_api_client.GoogleAPIClient;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class SpreadsheetCreator {
+
+    private final String spreadsheetId;
+    private final String spreadsheetTitle;
+    private final String projectId;
+    private final GoogleAPIClient service;
+    private final String folderId;
+
+    public SpreadsheetCreator(String spreadsheetTitle, String projectId, String folderId) throws IOException, GeneralSecurityException {
+        this.spreadsheetTitle = spreadsheetTitle;
+        this.projectId = projectId;
+        this.folderId = folderId;
+        this.service = new GoogleAPIClient();
+        this.spreadsheetId = setSpreadSheetId();
+    }
+
+    private String setSpreadSheetId() throws IOException {
+        if (service.searchFileInFolder(this.spreadsheetTitle, folderId).isEmpty()) {
+            return service.createSpreadSheet(this.spreadsheetTitle, folderId);
+        }
+        return service.getFileId(this.spreadsheetTitle, folderId);
+    }
+
+    public String getSpreadsheetId() {
+        return spreadsheetId;
+    }
+
+    public String getSpreadsheetTitle() {
+        return spreadsheetTitle;
+    }
+
+    public String getProjectId() {
+        return projectId;
+    }
+
+    public void deleteDefaultSheet() throws IOException {
+        this.service.deleteSheet(spreadsheetId, "Sheet1");
+    }
+}
